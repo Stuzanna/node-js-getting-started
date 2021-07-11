9fca83a02c29df5cfea5ca47ca798ebaa0925131
Stuzanna <66328800+Stuzanna@users.noreply.github.com>
2021-07-11 18:15:52 +0100

    Modifications to finish the tutorial
    Further info from the page: https://devcenter.heroku.com/articles/getting-started-with-nodejs#next-steps
    
    Next steps
    You now know how to deploy an app, change its configuration, view logs, scale, and attach add-ons.
    
    Here’s some recommended reading. The first, an article, will give you a firmer understanding of the basics. The second is a pointer to the main Node.js category here on Dev Center:
    
    Read How Heroku Works for a technical overview of the concepts you’ll encounter while writing, configuring, deploying and running applications.
    https://devcenter.heroku.com/articles/how-heroku-works
    
    Visit the Node.js category to learn more about developing and deploying Node.js applications.
    https://devcenter.heroku.com/categories/nodejs-support
    
    Read Deploying Node.js Apps on Heroku to understand how to take an existing Node.js app and deploy it to Heroku.
    https://devcenter.heroku.com/articles/deploying-nodejs
    
    Update index.js
    
    forgot to include this

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const cool = require('cool-ascii-faces');
 const express = require('express');
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -10,3 +18,25 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req,res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/times', (req, res) => res.send(showTimes()))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
+  showTimes = () => {
+    let result = '';
+    const times = process.env.TIMES || 5;
+    for (i = 0; i < times; i++) {
+      result += i + ' ';
+    }
+    return result;
+  }
